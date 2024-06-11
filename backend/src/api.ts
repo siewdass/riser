aa4497c550340608e214ef4cad0f84ee9a6f83bd
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-11 18:48:12 -0400

    x

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,4 +1,19 @@
 export async function API( database, req, res ) {
+
+	try {
+		const params = req.path.split( '/' )
+		const project = await database.Project.findOne( { name: params[ 2 ] } )
+
+		if ( !project ) throw 'Project not exist.'
+
+		console.log( project.name )
+
+	} catch ( error ) {
+
+		console.error( error )
+		
+	}
+
 	console.log( req.path.split( '/' ) )
 	res.json( { path: req.path.split( '/' ) } )
 }
