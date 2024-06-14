e3cba0b2e0fdaef05a4038dc95bf79f943e5ea85
Samuel Siewdass <samuel.siewdass@itsense.com.co>
2024-06-14 12:38:19 -0400

    x

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,6 @@
 import { createConnection } from 'mongoose'
 
-export class Database {
+/*export class Database {
 	db
 	async connect( url ) {
 		this.db = createConnection( url )
@@ -8,4 +8,24 @@ export class Database {
 	async find( collection ) {
 		await this.db.collection( collection ).find().toArray()
 	}
+}*/
+
+export async function Database( database, { body }, res ) {
+
+	try {
+
+		const project = await database.Project.findOne( { name: body.project } ) 
+		if ( !project ) throw `project ${ body.project } not exist.` 
+
+		const conn2 = createConnection( `mongodb://localhost/${ project.name }` )
+		console.log(conn2)
+		res.json( {} )
+
+	} catch ( error ) {
+		
+		console.error( error )
+		res.json( { error } )
+
+	}
+
 }
